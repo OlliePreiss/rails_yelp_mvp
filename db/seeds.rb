8169869462c970118e62833c08779b795027b99f
Ollie Preiss <76745659+OlliePreiss@users.noreply.github.com>
2023-05-02 20:23:07 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+# puts "Creating restaurants..."
+# 100.times do
+#   category = ["chinese", "italian", "japanese", "french", "belgian"]
+#   restaurant = {
+#     name: Faker::Restaurant.name,
+#     address: Faker::Address.street_address,
+#     phone_number: Faker::PhoneNumber.phone_number,
+#     category: category[rand(0..4)]
+#   }
+#   restaurant = Restaurant.create!(restaurant)
+#   puts "Created #{restaurant.name}"
+# end
+
+# puts "Finished"
+
+puts "Cleaning reviews database..."
+Review.destroy_all
+
+puts "Creating reviews..."
+
+restaurants = Restaurant.all
+
+200.times do
+  Review.create!(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5),
+    restaurant: restaurants.sample
+  )
+end
+
+puts "Created review"
+
+
+puts "Finished"
